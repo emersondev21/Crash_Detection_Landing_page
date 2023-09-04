e2051310afee37ab58807bf413a5086f5f954952
Emerson Batista <84859510+emersongg21@users.noreply.github.com>
2023-09-04 23:22:06 +0000

    '-'

--- assets/script/p3.js
+++ assets/script/p3.js
@@ -1,19 +1,19 @@
 var originalTitle = document.title;
 
 window.onscroll = function () {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("scroll-to-top").style.display = "block";
-    } else {
-        document.getElementById("scroll-to-top").style.display = "none";
-    }
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("scroll-to-top").style.display = "block";
+	} else {
+		document.getElementById("scroll-to-top").style.display = "none";
+	}
 };
 
 document.getElementById("scroll-to-top").addEventListener("click", function () {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
 
 function menuDrop() {
-    var drop = document.getElementById("nav2");
-    drop.classList.toggle("menu-drop");
+	var drop = document.getElementById("nav2");
+	drop.classList.toggle("menu-drop");
 }
---
+++ assets/script/scrollReveal.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal({ reset: true })
+
+ScrollReveal().reveal('.home', {
+    delay: 250
+});
+
+ScrollReveal().reveal('.projeto', {
+    delay: 250
+});
+
+ScrollReveal().reveal('.objetivo', {
+    delay: 250
+});
+
+ScrollReveal().reveal('.demo_contato', {
+    delay: 250
+});
+
+ScrollReveal().reveal('.time', {
+    delay: 250
+});
+
+ScrollReveal().reveal('.teste', {
+    delay: 250
+});
